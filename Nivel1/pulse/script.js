f176e3c82407d2c93c6a01063287027d91f0da98
donSebamarquez <sebamarquezblanco@gmail.com>
2025-01-30 16:34:00 -0300

    fixing navbar mobile

--- Nivel1/pulse/script.js
+++ Nivel1/pulse/script.js
@@ -1,10 +1,12 @@
 const menubtn = document.querySelector('.navbar__menu');
+const menutranslate = document.querySelector(".navbar__translate")
 const menulinks = document.querySelector('.navbar__links');
 const main = document.querySelector('.card__main');
 const footer = document.querySelector('.card__footer');
 
 menubtn.addEventListener("click", function() {
-    menulinks.classList.toggle('active');  
+    menulinks.classList.toggle('active');
+    menutranslate.classList.toggle('active')
     main.classList.toggle('deactive');     
     footer.classList.toggle('deactive'); 
 });
